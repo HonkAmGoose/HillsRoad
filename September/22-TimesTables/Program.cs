022931be72b8e24fd482b6706fdcdb831183f19d
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2022-09-22 17:15:36 +0100

    bug fix timestables

--- September/22-TimesTables/Program.cs
+++ September/22-TimesTables/Program.cs
@@ -9,7 +9,7 @@ namespace TimesTables
             Console.WriteLine("Enter a times table to generate:");
             int table = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 1; i < 12; i++)
+            for (int i = 1; i <= 12; i++)
             {
                 Console.WriteLine($"{i} x {table} = {i * table}");
             }
