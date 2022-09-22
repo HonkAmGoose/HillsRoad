b301b3e566bf1c30b3c7044014da0a14537c28cb
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2022-09-22 16:00:33 +0100

    Add times table and update password

---
+++ September/22-Password/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TimesTab
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string SecretPassword = "Password1";
+
+            int tries = 0;
+            bool incorrect = true;
+
+            Console.WriteLine("I am thinking of a (not) VERY secure password. See if you can guess it!");
+
+            do
+            {
+                Console.Write("Guess:");
+
+
+                tries++;
+            } while (incorrect && tries < 3);
+        }
+    }
+}
---
+++ September/22-TimesTables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TimesTables
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a times table to generate:");
+            int table = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < 12; i++)
+            {
+                Console.WriteLine($"{i} x {table} = {i * table}");
+            }
+        }
+    }
+}
