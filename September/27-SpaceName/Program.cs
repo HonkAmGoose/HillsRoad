2207167db64cdeee80025db70b5f0cf68df161ed
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2022-09-27 11:05:09 +0100

    Add SpaceName project

---
+++ September/27-SpaceName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SpaceName
+{
+    class Program
+    {
+        public static string Input(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
+        }
+        public static void Main(string[] args)
+        {
+            string firstName = Input("What is your first name?");
+            string lastName = Input("What is your last name?");
+            string town = Input("What town were you born in?");
+            string month = Input("What month were you born in?");
+
+            Console.WriteLine($"Your space first name is {lastName.Substring(0, 3) + firstName.Substring(0, 2)}");
+            Console.WriteLine($"Your space last name is {town.Substring(0, 3) + month.Substring(month.Length - 2, 2)}");
+        }
+    }
+}
