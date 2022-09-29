411b5ad64c34652934dd657a3539175f95ae9015
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2022-09-29 15:18:59 +0100

    Add LetterCounter project

---
+++ September/29-3LetterCounter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LetterCounter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to see how many steps it requires to reduce to zero:");
+            
+        }
+    }
+}
