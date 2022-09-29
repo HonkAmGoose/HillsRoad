ec0aa8ba8343ca249ccdd6a5be050b9f6462c3ea
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2022-09-29 15:17:34 +0100

    Add FibbonacciGen

---
+++ September/FibonnaciGen/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FibbonacciGen
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter how many terms of the fibbonacci sequence you would like to generate");
+            int num = Convert.ToInt32(Console.ReadLine());
+            UInt64 x = 0;
+            UInt64 y = 1;
+
+            Console.Write(x + ", ");
+            for (int i = 0; i < num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    y += x;
+                    Console.Write(y + ", ");
+                }
+                else if (i % 2 == 1)
+                {
+                    x += y;
+                    Console.Write(x + ", ");
+                }
+            }
+        }
+    }
+}
