be6640115a82846630b5bf25f32877d5258c198e
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2022-09-29 15:05:05 +0100

    Create reducenumber project

---
+++ September/29-ReduceNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReduceNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to see how many steps it requires to reduce to zero:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int steps = 0;
+
+            while (num > 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                }
+                else if (num % 2 == 1)
+                {
+                    num -= 1;
+                }
+                else
+                {
+                    throw new Exception("Something went wrong");
+                }
+                steps++;
+            }
+
+            Console.WriteLine($"It took {steps} steps to reduce to 0");
+        }
+    }
+}
