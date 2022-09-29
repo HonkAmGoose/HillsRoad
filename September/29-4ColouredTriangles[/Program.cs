157bfb238194dd02177fa974f86994650c2eac81
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2022-09-29 16:00:23 +0100

    NOT WORKING continued ColouredTriangles

--- September/29-4ColouredTriangles[/Program.cs
+++ September/29-4ColouredTriangles[/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace ColouredTriangles
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ColouredTriangles
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Get input
+            Console.WriteLine("Enter the first line of the triangle");
+            string input = Console.ReadLine();
+            int length = input.Length;
+            char[] Colours = { 'R', 'G', 'B' };
+            List<char> colours;
+
+            // Convert input to array
+            char[] line = Array.Empty<char>();
+            foreach (char c in input)
+            {
+                line.Append(c);
+            }
+
+            for (int i = 1; i <= length; i++)
+            {
+                for (int j = 0; j < length - i; j++)
+                {
+                    if (line[j] != line[j + 1])
+                    {
+                        colours = Colours.ToList();
+                        colours.Remove(line[j]);
+                        colours.Remove(line[j + 1]);
+                        line[j] = colours[0];
+                    }
+                }
+            }
         }
     }
 }
