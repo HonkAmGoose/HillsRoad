60ce9d5bd03bd1dfac814b1ebb9e8329c3f10ffd
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2022-10-18 10:54:31 +0100

    Add other functions

--- October/18-1Calculator/Program.cs
+++ October/18-1Calculator/Program.cs
@@ -6,38 +6,75 @@
         {
             Console.WriteLine("Welcome to the calculator program\n");
 
-            // Ask the user for two number and store them as floats
-            Console.WriteLine("Enter the first number: ");
-            float number1 = float.Parse(Console.ReadLine());
+            bool again = true;
 
-            Console.WriteLine("Enter the second number: ");
-            float number2 = float.Parse(Console.ReadLine());
-
-            // Output the menu options
-            Console.WriteLine("\nEnter the menu number of the calculation to perform: ");
-            Console.WriteLine("1 - Addition");
-            Console.WriteLine("2 - Subtraction");
-            Console.WriteLine("3 - Multiplication");
-            Console.WriteLine("4 - Division\n");
-
-            // Ask for the menu option
-            string menuOption = Console.ReadLine();
-
-            // Perform a subroutine based on the menu option
-            if (menuOption == "1")
+            while (again)
             {
-                Addition(number1, number2);
-            }
-            else
-            {
-                Console.WriteLine("Please choose a valid option");
+                // Ask the user for two number and store them as floats
+                Console.WriteLine("Enter the first number: ");
+                float number1 = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the second number: ");
+                float number2 = float.Parse(Console.ReadLine());
+
+                // Output the menu options
+                Console.WriteLine("\nEnter the menu number of the calculation to perform: ");
+                Console.WriteLine("1 - Addition\n");
+                Console.WriteLine("2 - Subtraction\n");
+                Console.WriteLine("3 - Multiplication\n");
+                Console.WriteLine("4 - Division\n");
+                Console.WriteLine("q - Quit\n");
+
+                // Ask for the menu option
+                string menuOption = Console.ReadLine();
+
+                // Perform a subroutine based on the menu option
+                switch (menuOption)
+                {
+                    case "1":
+                        Addition(number1, number2);
+                        break;
+
+                    case "2":
+                        Subtraction(number1, number2);
+                        break;
+
+                    case "3":
+                        Division(number1, number2);
+                        break;
+
+                    case "4":
+                        Multiplication(number1, number2);
+                        break;
+
+                    case "q":
+                        again = false;
+                        break;
+    
+                    default:
+                        Console.WriteLine("Enter a valid menu option");
+                        break;
+                }
             }
         }
+
 
         // The addition procedure has two floats as parameters, adds them together and outputs the result
         static void Addition(float num1, float num2)
         {
-            Console.WriteLine("\nThe result is: " + num1 + num2);
+            Console.WriteLine("\nThe result is: " + (num1 + num2));
+        }
+        static void Subtraction(float num1, float num2)
+        {
+            Console.WriteLine("\nThe result is: " + (num1 - num2));
+        }
+        static void Multiplication(float num1, float num2)
+        {
+            Console.WriteLine("\nThe result is: " + (num1 * num2));
+        }
+        static void Division(float num1, float num2)
+        {
+            Console.WriteLine("\nThe result is: " + (num1 / num2));
         }
     }
 }
