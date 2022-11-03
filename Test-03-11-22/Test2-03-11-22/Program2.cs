00ed70b807692b771f28979ac3f9cf953f91ac64
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2022-11-03 15:39:15 +0000

    Did the test

---
+++ Test-03-11-22/Test2-03-11-22/Program2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Test2
+{
+    class Program2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("The first few prime numbers are:");
+
+            int count2;
+            string prime;
+            for (int count1 = 2; count1 < 51; count1++)
+            {
+                count2 = 2;
+                prime = "Yes";
+                while (count2 * count2 <= count1)
+                {
+                    if (count1 % count2 == 0)
+                    {
+                        prime = "No";
+                    }
+                    count2 += 1;
+                }
+                if (prime.Equals("Yes"))
+                {
+                    Console.WriteLine(count1);
+                }
+            }
+        }
+    }
+}
