614d89f41d70c86d62a6bb1e6c85e87689b6f904
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2022-11-22 10:50:23 +0000

    Added passing by reference and value

---
+++ November/22-1PassingByValue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PassingByValue
+{
+    class Program
+    {
+        static void Main()
+        {
+            double pay;
+            pay = 2000;
+
+            CalculatePay(pay);
+            Console.WriteLine(pay);
+        }
+
+        // Function to calculate deductions from pay
+        static double SubtractDeductions(double pay, double percent)
+        {
+            return (pay * percent) / 100;
+        }
+
+        static double CalculatePay(double pay)
+        {
+            // Subtract tax on pay at 22%
+            return pay - SubtractDeductions(pay, 22);
+        }
+    }
+}
---
+++ November/22-2PassingByReference/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PassingByReference
+{
+    class Program
+    {
+        static void Main()
+        {
+            double pay;
+            pay = 2000;
+
+            CalculatePay(ref pay);
+            Console.WriteLine(pay);
+        }
+
+        // Function to calculate deductions from pay
+        static double SubtractDeductions(double pay, double percent)
+        {
+            return (pay * percent) / 100;
+        }
+
+        static void CalculatePay(ref double pay)
+        {
+            // Subtract tax on pay at 22%
+            pay = pay - SubtractDeductions(pay, 22);
+        }
+    }
+}
