1bc1d6a9e5035afe3c66927f6ac40f3e9ee48226
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2022-11-24 14:58:39 +0000

    Add passwords project

---
+++ November/22-4Passwords/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Passwords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filename = "passwords.txt";
+
+            string username = GetStrInput("Enter the username: ");
+            string password = GetStrInput("Enter the password: ");
+
+            using (StreamWriter sw = new StreamWriter(filename, true))
+            {
+                sw.WriteLine(username);
+                sw.WriteLine(password);
+            }
+
+            Console.WriteLine();
+
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+        static string GetStrInput(string enterPrompt)
+        {
+            Console.WriteLine(enterPrompt);
+            return Console.ReadLine();
+        }
+    }
+}
