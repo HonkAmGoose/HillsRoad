4de1f27fd70b02f8cf21cb576b14f7da4a1e519a
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2022-12-15 16:01:24 +0000

    update, outputgrid currently broken

--- BattleBoats/BattleBoats/Program.cs
+++ BattleBoats/BattleBoats/Program.cs
@@ -4,12 +4,12 @@ namespace BattleBoats
 {
     public class Program
     {
-        public char[,] PlayerFleetGrid = new char[8,8];
-        public char[,] PlayerTargetTracker = new char[8, 8];
-        public char[,] ComputerFleetGrid = new char[8, 8];
-        public char[,] ComputerTargetTracker = new char[8, 8];
+        static char[,] PlayerFleetGrid = new char[8,8];
+        static char[,] PlayerTargetTracker = new char[8, 8];
+        static char[,] ComputerFleetGrid = new char[8, 8];
+        static char[,] ComputerTargetTracker = new char[8, 8];
 
-        public void Main(String[] args)
+        public static void Main(String[] args)
         {
             // Setting up constants
             string[] menuOptions =
@@ -20,9 +20,9 @@ namespace BattleBoats
                 "Quit game"
             }; // MAKE SURE TO UPDATE SWITCH CASE STATEMENT WHEN UPDATING MENUoPTIONS
             const string WelcomePrompt = "Welcome to this Battle Boats game!";
-            const string EnterPrompt = "Please select what you would like to do:";
-            const string FormatErrorPrompt = "Sorry, you need to enter an integer";
-            const string OutOfBoundErrorPrompt = "Sorry, you need to enter a menu option";
+            const string MenuEnterPrompt = "Please select what you would like to do:";
+            const string MenuFormatErrorPrompt = "Sorry, you need to enter an integer";
+            const string MenuOutOfBoundErrorPrompt = "Sorry, you need to enter a menu option";
             const string ExitPrompt = "Thank you for playing, come back again soon!\nPress any key to close this window...";
 
             // Setting up variables
@@ -31,7 +31,7 @@ namespace BattleBoats
             // Main loop until user wants to quit
             while (cont)
             {
-                switch (Menu(WelcomePrompt, EnterPrompt, FormatErrorPrompt, OutOfBoundErrorPrompt, menuOptions))
+                switch (Menu(WelcomePrompt, MenuEnterPrompt, MenuFormatErrorPrompt, MenuOutOfBoundErrorPrompt, menuOptions))
                 {
                     case 1:
                         PlayNewGame();
@@ -50,15 +50,72 @@ namespace BattleBoats
                         break;
 
                     default:
-                        throw new Exception("This should be unreachable (GetIntInput() within Menu() should ensure return value is within constraints)");
-                }// MAKE SURE TO UPDATE MENUoPTIONS WHEN UPDATING SWITCH CASE STATEMENT
+                        throw new ArgumentOutOfRangeException("This should be unreachable (GetIntInput() within Menu() should ensure return value is within constraints)");
+                }// MAKE SURE TO UPDATE MENUOPTIONS WHEN UPDATING SWITCH CASE STATEMENT
             }
             Console.WriteLine(ExitPrompt);
             Console.ReadKey(); // Wait for user to press a key to exit
         }
-        static void OutputGrid(char[,] grid)
+        static void OutputGrid(char[,] grid) // Function to output grid in a pretty format
         {
+            Console.WriteLine(" |A|B|C|D|E|F|G|H");
             for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                Console.WriteLine("-----------------");
+                Console.Write(i);
+                for (int j = 0; j < grid.GetLength(1); i++)
+                {
+                    Console.Write($"|{grid[i, j]}");
+                }
+            }
+        }
+        static int ConvertXCoord(char coord) // Function to convert a letter X coordinate to number, throwing FormatException if not between A and H
+        {
+            if ('A' <= coord && coord <= 'H')
+            {
+                return (int)coord - 65;
+            }
+            else
+            {
+                throw new FormatException("This function only takes in a char between A and H");
+            }
+        }
+        static int[] GetCoordinateInput(string enterPrompt, string formatErrorPrompt, string alreadyExistsErrorPrompt, char[,] toCheck) // Function to get coordinate input between A1 and H8 if not already on grid
+        {
+            Console.WriteLine(enterPrompt);
+            string response;
+            int row, col;
+            while (true)
+            {
+                response = Console.ReadLine().ToUpper();
+                try
+                {
+                    if (response.Length != 2)
+                    {
+                        throw new FormatException();
+                    }
+                    col = ConvertXCoord(response[0]); // Throws FormatException if not between A and H
+                    row = Convert.ToInt32(response[1]); // Throws FormatException if not integer
+                }
+                catch (FormatException) // Prompt to try again
+                {
+                    Console.WriteLine(formatErrorPrompt);
+                    continue;
+                }
+                if (col < 1 | col > 8) // Prompt to try again if not within bounds
+                {
+                    Console.WriteLine(formatErrorPrompt);
+                    continue;
+                }
+                if (toCheck[col,row] != ' ')
+                {
+                    Console.WriteLine(alreadyExistsErrorPrompt);
+                    continue;
+                }
+                break; // If it makes it to this point, the input is valid so break out of loop
+            }
+            int[] value = { col, row };
+            return value;
         }
         static int GetIntInput(string enterPrompt, string formatErrorPrompt, string outOfBoundErrorPrompt, int lowerBound, int upperBound) // Function to get inclusive bounded integer input
         {
@@ -102,8 +159,48 @@ namespace BattleBoats
             PlayGame();
         }
         static void SetupNewGame() 
-        { 
+        {
+            const string PlaceEnterPrompt = "Please enter the coordinate to place a boat";
+            const string CoordinateFormatErrorPrompt = "Sorry, enter a coordinate between A1 and H8";
+            const string AlreadyExistsErrorPrompt = "Sorry, a boat already exists at that coordinate";
+            List<int[]> allCoords = new List<int[]>();
 
+            // Initialise all char[] to contain spaces and coords to contain each coordinate
+            int[] coord = new int[2];
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    PlayerFleetGrid[i, j] = ' ';
+                    PlayerTargetTracker[i, j] = ' ';
+                    ComputerFleetGrid[i, j] = ' ';
+                    ComputerTargetTracker[i, j] = ' ';
+
+                    coord[0] = i;
+                    coord[1] = j;
+                    allCoords.Append(coord);
+                }
+            }
+
+            // Get 5 boat spaces for the player
+            for (int i = 0; i < 5; i++)
+            {
+                OutputGrid(PlayerFleetGrid);
+                coord = GetCoordinateInput(PlaceEnterPrompt, CoordinateFormatErrorPrompt, AlreadyExistsErrorPrompt, PlayerFleetGrid);
+                PlayerFleetGrid[coord[0], coord[1]] = 'B';
+            }
+
+            Random rand = new();
+            for (int i = 0; i < 5; i++)
+            {
+                coord = allCoords[rand.Next(allCoords.Count)];
+                ComputerFleetGrid[coord[0], coord[1]] = 'B';
+                allCoords.Remove(coord);
+            }
+            // FOR TESTING ONLY, output computer fleet grid
+            OutputGrid(ComputerFleetGrid);
+
+            return;
         }
         static void ResumeSavedGame()  // Function to play a game from a save file 
         {
