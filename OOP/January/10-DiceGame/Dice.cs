42434737c3485d9bcbecbc76e6fc808474e561b2
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2023-01-10 11:19:15 +0000

    Added dice game with code from sharepoint

---
+++ OOP/January/10-DiceGame/Dice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DiceGame
+{
+    public class Dice
+    {
+        public int value;
+        public int sides;
+        Random rnd = new Random();
+
+        public Dice(int numSides)
+        {
+            sides = numSides;
+        }
+
+        public int getValue()
+        {
+            return value;
+        }
+
+        public void Roll()
+        {
+            value = rnd.Next(sides) + 1;
+        }
+    }
+}
---
+++ OOP/January/10-DiceGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice myDice = new Dice(6);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Rolling the dice...");
+                myDice.Roll();
+                Console.WriteLine($"The dice shows: {myDice.getValue()}");
+            }
+        }
+    }
+}
