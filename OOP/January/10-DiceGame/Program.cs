04d9fb67d7e3ba7f3ce3a7d03e37a97b5653830c
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2023-01-10 11:28:46 +0000

    start frequency tracking

--- OOP/January/10-DiceGame/Program.cs
+++ OOP/January/10-DiceGame/Program.cs
@@ -6,13 +6,25 @@ namespace DiceGame
     {
         static void Main(string[] args)
         {
-            Dice myDice = new Dice(6);
+            // Create object of class dice with specified number of sides
+            Console.WriteLine("Enter the number of sides you would like the dice to have:");
+            int sides = Convert.ToInt32(Console.ReadLine());
+            Dice myDice = new Dice(sides);
 
-            for (int i = 0; i < 5; i++)
+            // Create int[] of length specified
+            Console.WriteLine("Enter how many rolls you would like to simulate:");
+            int simulations = Convert.ToInt32(Console.ReadLine());
+            int[] rolls = new int[simulations];
+
+            // Create dictionary to track frequency of each roll
+            Dictionary<int, int> frequency = new Dictionary<int, int>();
+
+            // Loop for number specified and add to array
+            for (int i = 0; i < simulations; i++)
             {
-                Console.WriteLine("Rolling the dice...");
+                Console.WriteLine($"Roll {i}");
                 myDice.Roll();
-                Console.WriteLine($"The dice shows: {myDice.getValue()}");
+                rolls[i] = myDice.getValue();
             }
         }
     }
