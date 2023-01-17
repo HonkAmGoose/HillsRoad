3e652c47ced24ebf15eba86673e1d2473ff46f94
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2023-01-17 10:39:27 +0000

    Add BankAccount homework

---
+++ OOP/January/13-BankAccount/Account.cs
@@ -0,0 +1,46 @@
+﻿namespace BankAccounts
+{
+    public class Account
+    {
+        private string acctHolder;
+        private string acctNumber;
+        private double currentBalance;
+
+        public Account(string name, string number, double balance)
+        {
+            this.acctHolder = name;
+            this.acctNumber = number;
+            this.currentBalance = balance;
+        }
+
+        public void Deposit(double amount)
+        {
+            this.currentBalance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            this.currentBalance -= amount;
+        }
+
+        public double GetBalance()
+        {
+            return this.currentBalance;
+        }
+
+        public string GetAcctHolder()
+        {
+            return this.acctHolder;
+        }
+
+        public string GetAcctNumber()
+        {
+            return this.acctNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"Account number {this.acctNumber}, held by {this.acctHolder}, with balance {this.currentBalance}";
+        }
+    }
+}
---
+++ OOP/January/13-BankAccount/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BankAccounts
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Account acct = new("Sam", "1", 0);
+            Console.WriteLine(acct.ToString());
+            acct.Deposit(50);
+            Console.WriteLine(acct.GetBalance());
+            acct.Withdraw(20.50);
+            Console.WriteLine(acct.GetBalance());
+        }
+    }
+}
