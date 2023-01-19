20d3546229006f55399c563250aed9e635eac700
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2023-01-19 15:58:13 +0000

    initial commit for liardice

---
+++ OOP/January/19-LiarsDice/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LiarsDice
+{
+    internal class Game
+    {
+        private Hand[] hands;
+        public Game(int numberOfHands, int dicePerPlayer, int sidesPerDice)
+        {
+            hands = new Hand[numberOfHands];
+            for (int i = 0; i < numberOfHands; i++)
+            {
+                hands[i] = new Hand(dicePerPlayer, sidesPerDice);
+            }
+
+            foreach(Hand hand in hands)
+            {
+                foreach(Die die in hand)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ OOP/January/19-LiarsDice/Hand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LiarsDice
+{
+    internal class Hand
+    {
+        private Die[] dice;
+        public Hand(int numberOfDice, int sidesPerDice)
+        {
+            dice = new Die[numberOfDice];
+            for (int i = 0; i < numberOfDice; i++)
+            {
+                dice[i] = new Die(sidesPerDice);
+            }
+        }
+    }
+}
