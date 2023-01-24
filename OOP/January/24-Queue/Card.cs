214c04c33dc22c68fc8326a8b2a74846b4b20005
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2023-01-24 10:58:26 +0000

    as given

---
+++ OOP/January/24-Queue/Card.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCardGameV2._0
+{
+    public class Card
+    {
+        private int rank;
+        private int suit;
+
+        public Card(int r, int s)
+        {
+            rank = r;
+            suit = s;
+        }
+
+        public int GetRank()
+        {
+            return rank;
+        }
+
+        public int GetSuit()
+        {
+            return suit;
+        }
+
+        public int GetScore()
+        {
+            int score = rank * 4 + suit;
+            return score;
+        }
+
+        public string GetRankAsString()
+        {
+            string[] ranks = { "Ace", "Two", "Three", "Four", "Five",
+              "Six","Seven", "Eight", "Nine", "Ten",
+              "Jack","Queen", "King" };
+            return ranks[rank - 1];
+        }
+
+        public string GetSuitAsString()
+        {
+            string[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+            return suits[suit];
+        }
+
+        public string GetName()
+        {
+            return GetRankAsString() + " of " + GetSuitAsString();
+        }
+
+    }
+}
---
+++ OOP/January/24-Queue/Pack.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCardGameV2._0
+{
+    public class Pack
+    {
+        Random rnd = new Random();
+        private Card[] cardsArray = new Card[52];
+
+        private int front;
+        private int rear;
+
+        private int size;
+        // size is number of cards currently in pack
+        public int Size //property to provide read-only access to size field
+        {
+            get { return size; }
+        }
+
+        public Pack() //constructor
+        {
+            for (int i = 0; i <= 51; i++)
+            {
+                cardsArray[i] = new Card((i % 13) + 1, i / 13);
+            }
+            //!!!!!!!
+            // initialise the three integers below
+            front = //???;
+            rear = //???;
+            size = //???;
+        }
+
+        public void Shuffle()
+        {
+            //Fisher-Yates shuffle
+            for (int i = 0; i < size - 1; i++)
+            {
+                int r = rnd.Next(i + 1, 52);
+                Card temp = cardsArray[i];
+                cardsArray[i] = cardsArray[r];
+                cardsArray[r] = temp;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+
+        public bool IsFull()
+        {
+            return size == 52;
+        }
+
+        public Card DealCard()
+        {
+            if (!IsEmpty())
+            {
+                Card ACard = cardsArray[front];
+                if (front == 51)
+                    front = 0;
+                else
+                    front++;
+                size--;
+                return ACard;
+            }
+            else
+                return null;
+        }
+
+        public void AddCard(Card ACard)
+        {
+            // !!!!
+            // write the code for this method (see the slideshow for the algorithm)
+            // !!!!
+            
+        }
+    }
+}
