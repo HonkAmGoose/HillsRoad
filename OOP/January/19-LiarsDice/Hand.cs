89ea2206a15758b3c0b44fffaa720af9ddde7912
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2023-01-28 15:04:07 +0000

    liars dice finish

--- OOP/January/19-LiarsDice/Hand.cs
+++ OOP/January/19-LiarsDice/Hand.cs
@@ -19,14 +19,14 @@ namespace LiarsDice
             return dice;
         }
 
-        public bool PlayerTurn(bid current)
+        public bool PlayerTurn(Bid current)
         {
             Console.WriteLine(
                 $"The current bid is {current.GetNum()} {current.GetRoll()}s\n" +
                 "To make a new bid, type a number and roll separated by a space (both in numerals)\n" +
                 "To challenge the current bid, type 'Liar'"
                 );
-            bidInput = Console.ReadLine();
+            string bidInput = Console.ReadLine();
 
             if (bidInput.ToLower().Equals("liar"))
             {
