45297eec62aec4c00db2cf8ec84f3dbca3a50bec
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2023-02-02 15:22:11 +0000

    Add empty classes

---
+++ Connect4/Connect4/Player.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Connect4
+{
+    internal class Player
+    {
+    }
+}
---
+++ Connect4/Connect4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Connect4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
