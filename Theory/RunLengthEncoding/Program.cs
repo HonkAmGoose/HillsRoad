307f742be64308007057100003e4b686cdc3940d
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2023-02-28 10:03:07 +0000

    ADDED rle project, currently broken

--- Theory/RunLengthEncoding/Program.cs
+++ Theory/RunLengthEncoding/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace RLE
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Input some text to compress");
+            string original = Console.ReadLine();
+            int running = 1;
+            for (int i = 0; i < original.Length - 1; i++)
+            {
+                if (original[i] == original[i + 1])
+                {
+                    running++;
+                }
+                else if (i == original.Length - 2)
+                {
+                    if (original[i] == original[i + 1])
+                    {
+                        running++;
+                        Console.WriteLine($"{original[i]} {running}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{original[i]} {running} {original[i + 1]} 1");
+                    }
+                }
+                else
+                {
+                    Console.Write($"{original[i]} {running} ");
+                    running = 1;
+                }
+            }
+        }
+    }
+}
