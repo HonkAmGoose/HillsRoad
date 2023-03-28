8638d136ba12c5a99cbd88ae1370f7dd362fe945
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2023-03-28 11:28:37 +0100

    add all buttons

--- Event Driven/Calculator/CalculatorForm.Designer.cs
+++ Event Driven/Calculator/CalculatorForm.Designer.cs
@@ -29,17 +29,21 @@
         private void InitializeComponent()
         {
             this.AddButton = new System.Windows.Forms.Button();
-            this.Operand1 = new System.Windows.Forms.TextBox();
-            this.Operand2 = new System.Windows.Forms.TextBox();
-            this.Result = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.Operand1TextBox = new System.Windows.Forms.TextBox();
+            this.Operand2TextBox = new System.Windows.Forms.TextBox();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.Operand1Label = new System.Windows.Forms.Label();
+            this.Operand2Label = new System.Windows.Forms.Label();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.MultiplyButton = new System.Windows.Forms.Button();
+            this.SubtractButton = new System.Windows.Forms.Button();
+            this.DivideButton = new System.Windows.Forms.Button();
+            this.RoundCheckBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // AddButton
             // 
-            this.AddButton.Location = new System.Drawing.Point(391, 124);
+            this.AddButton.Location = new System.Drawing.Point(274, 61);
             this.AddButton.Name = "AddButton";
             this.AddButton.Size = new System.Drawing.Size(75, 23);
             this.AddButton.TabIndex = 0;
@@ -47,68 +51,115 @@
             this.AddButton.UseVisualStyleBackColor = true;
             this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
-            // Operand1
+            // Operand1TextBox
             // 
-            this.Operand1.Location = new System.Drawing.Point(137, 61);
-            this.Operand1.Name = "Operand1";
-            this.Operand1.Size = new System.Drawing.Size(100, 20);
-            this.Operand1.TabIndex = 1;
+            this.Operand1TextBox.Location = new System.Drawing.Point(137, 61);
+            this.Operand1TextBox.Name = "Operand1TextBox";
+            this.Operand1TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Operand1TextBox.TabIndex = 1;
             // 
-            // Operand2
+            // Operand2TextBox
             // 
-            this.Operand2.Location = new System.Drawing.Point(137, 103);
-            this.Operand2.Name = "Operand2";
-            this.Operand2.Size = new System.Drawing.Size(100, 20);
-            this.Operand2.TabIndex = 2;
+            this.Operand2TextBox.Location = new System.Drawing.Point(137, 103);
+            this.Operand2TextBox.Name = "Operand2TextBox";
+            this.Operand2TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Operand2TextBox.TabIndex = 2;
             // 
-            // Result
+            // ResultTextBox
             // 
-            this.Result.Location = new System.Drawing.Point(137, 151);
-            this.Result.Name = "Result";
-            this.Result.Size = new System.Drawing.Size(100, 20);
-            this.Result.TabIndex = 3;
+            this.ResultTextBox.Location = new System.Drawing.Point(137, 170);
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ResultTextBox.TabIndex = 3;
             // 
-            // label1
+            // Operand1Label
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 61);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Operand 1";
+            this.Operand1Label.AutoSize = true;
+            this.Operand1Label.Location = new System.Drawing.Point(51, 61);
+            this.Operand1Label.Name = "Operand1Label";
+            this.Operand1Label.Size = new System.Drawing.Size(57, 13);
+            this.Operand1Label.TabIndex = 4;
+            this.Operand1Label.Text = "Operand 1";
             // 
-            // label2
+            // Operand2Label
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 103);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Operand2";
+            this.Operand2Label.AutoSize = true;
+            this.Operand2Label.Location = new System.Drawing.Point(51, 103);
+            this.Operand2Label.Name = "Operand2Label";
+            this.Operand2Label.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Operand2Label.Size = new System.Drawing.Size(57, 13);
+            this.Operand2Label.TabIndex = 5;
+            this.Operand2Label.Text = "Operand 2";
             // 
-            // label3
+            // ResultLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(51, 151);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Result";
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(51, 170);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(37, 13);
+            this.ResultLabel.TabIndex = 6;
+            this.ResultLabel.Text = "Result";
+            // 
+            // MultiplyButton
+            // 
+            this.MultiplyButton.Location = new System.Drawing.Point(274, 103);
+            this.MultiplyButton.Name = "MultiplyButton";
+            this.MultiplyButton.Size = new System.Drawing.Size(75, 23);
+            this.MultiplyButton.TabIndex = 7;
+            this.MultiplyButton.Text = "Multiply";
+            this.MultiplyButton.UseVisualStyleBackColor = true;
+            this.MultiplyButton.Click += new System.EventHandler(this.MultiplyButton_Click);
+            // 
+            // SubtractButton
+            // 
+            this.SubtractButton.Location = new System.Drawing.Point(367, 61);
+            this.SubtractButton.Name = "SubtractButton";
+            this.SubtractButton.Size = new System.Drawing.Size(75, 23);
+            this.SubtractButton.TabIndex = 8;
+            this.SubtractButton.Text = "Subtract";
+            this.SubtractButton.UseVisualStyleBackColor = true;
+            this.SubtractButton.Click += new System.EventHandler(this.SubtractButton_Click);
+            // 
+            // DivideButton
+            // 
+            this.DivideButton.Location = new System.Drawing.Point(367, 103);
+            this.DivideButton.Name = "DivideButton";
+            this.DivideButton.Size = new System.Drawing.Size(75, 23);
+            this.DivideButton.TabIndex = 9;
+            this.DivideButton.Text = "Divide";
+            this.DivideButton.UseVisualStyleBackColor = true;
+            this.DivideButton.Click += new System.EventHandler(this.DivideButton_Click);
+            // 
+            // RoundCheckBox
+            // 
+            this.RoundCheckBox.AutoSize = true;
+            this.RoundCheckBox.Location = new System.Drawing.Point(274, 170);
+            this.RoundCheckBox.Name = "RoundCheckBox";
+            this.RoundCheckBox.Size = new System.Drawing.Size(64, 17);
+            this.RoundCheckBox.TabIndex = 10;
+            this.RoundCheckBox.Text = "Round?";
+            this.RoundCheckBox.UseVisualStyleBackColor = true;
+            this.RoundCheckBox.CheckedChanged += new System.EventHandler(this.RoundCheckBox_CheckedChanged);
             // 
             // CalculatorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.Result);
-            this.Controls.Add(this.Operand2);
-            this.Controls.Add(this.Operand1);
+            this.ClientSize = new System.Drawing.Size(693, 263);
+            this.Controls.Add(this.RoundCheckBox);
+            this.Controls.Add(this.DivideButton);
+            this.Controls.Add(this.SubtractButton);
+            this.Controls.Add(this.MultiplyButton);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.Operand2Label);
+            this.Controls.Add(this.Operand1Label);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.Operand2TextBox);
+            this.Controls.Add(this.Operand1TextBox);
             this.Controls.Add(this.AddButton);
             this.Name = "CalculatorForm";
             this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.CalculatorForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,12 +168,16 @@
         #endregion
 
         private System.Windows.Forms.Button AddButton;
-        private System.Windows.Forms.TextBox Operand1;
-        private System.Windows.Forms.TextBox Operand2;
-        private System.Windows.Forms.TextBox Result;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Operand1TextBox;
+        private System.Windows.Forms.TextBox Operand2TextBox;
+        private System.Windows.Forms.TextBox ResultTextBox;
+        private System.Windows.Forms.Label Operand1Label;
+        private System.Windows.Forms.Label Operand2Label;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button MultiplyButton;
+        private System.Windows.Forms.Button SubtractButton;
+        private System.Windows.Forms.Button DivideButton;
+        private System.Windows.Forms.CheckBox RoundCheckBox;
     }
 }
 
--- Event Driven/Calculator/CalculatorForm.cs
+++ Event Driven/Calculator/CalculatorForm.cs
@@ -17,7 +17,74 @@ namespace Calculator
             InitializeComponent();
         }
 
+        // Helper Functions //
+
+        private double fullAnswer = 0;
+
+        private double[] ParseOperands()
+        {
+            double operand1 = 0, operand2 = 0;
+            try
+            {
+                operand1 = double.Parse(Operand1TextBox.Text);
+                operand2 = double.Parse(Operand2TextBox.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid input");
+            }
+
+            return new double[] { operand1, operand2 };
+        }
+
+        private void OutputNewResult(double answer)
+        {
+            fullAnswer = answer;
+            OutputOldResult();
+        }
+
+        private void OutputOldResult()
+        {
+            double answer = fullAnswer;
+            if (RoundCheckBox.Checked == true)
+            {
+                answer = Math.Round(answer);
+            }
+            ResultTextBox.Text = answer.ToString();
+        }
+
+        // Event Functions //
+
         private void AddButton_Click(object sender, EventArgs e)
+        {
+            double[] operands = ParseOperands();
+            OutputNewResult(operands[0] + operands[1]);
+        }
+
+        private void SubtractButton_Click(object sender, EventArgs e)
+        {
+            double[] operands = ParseOperands();
+            OutputNewResult(operands[0] - operands[1]);
+        }
+
+        private void MultiplyButton_Click(object sender, EventArgs e)
+        {
+            double[] operands = ParseOperands();
+            OutputNewResult(operands[0] * operands[1]);
+        }
+
+        private void DivideButton_Click(object sender, EventArgs e)
+        {
+            double[] operands = ParseOperands();
+            OutputNewResult(operands[0] / operands[1]);
+        }
+
+        private void RoundCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            OutputOldResult();
+        }
+
+        private void CalculatorForm_Load(object sender, EventArgs e)
         {
 
         }
