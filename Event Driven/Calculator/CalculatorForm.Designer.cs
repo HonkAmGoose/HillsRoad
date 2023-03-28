96744c902b580448d014d78b377655c56ee5305a
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2023-03-28 10:48:40 +0100

    add event driven with initial project with some objects

---
+++ Event Driven/Calculator/CalculatorForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Calculator
+{
+    partial class CalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.Operand1 = new System.Windows.Forms.TextBox();
+            this.Operand2 = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(391, 124);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // Operand1
+            // 
+            this.Operand1.Location = new System.Drawing.Point(137, 61);
+            this.Operand1.Name = "Operand1";
+            this.Operand1.Size = new System.Drawing.Size(100, 20);
+            this.Operand1.TabIndex = 1;
+            // 
+            // Operand2
+            // 
+            this.Operand2.Location = new System.Drawing.Point(137, 103);
+            this.Operand2.Name = "Operand2";
+            this.Operand2.Size = new System.Drawing.Size(100, 20);
+            this.Operand2.TabIndex = 2;
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(137, 151);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(100, 20);
+            this.Result.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Operand 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Operand2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Result";
+            // 
+            // CalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Operand2);
+            this.Controls.Add(this.Operand1);
+            this.Controls.Add(this.AddButton);
+            this.Name = "CalculatorForm";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox Operand1;
+        private System.Windows.Forms.TextBox Operand2;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
