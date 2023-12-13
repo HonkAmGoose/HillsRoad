9780e91e4fc4db9db69e07c4715ee7ae62d8e083
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2023-12-13 16:48:39 +0000

    Add OthelloAPI

---
+++ OthelloAPI/OthelloAPI/Controllers/OthelloController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OthelloAPI.Controllers
+{
+    [Route("api/Othello")]
+    [ApiController]
+    public class OthelloController : ControllerBase
+    {
+        public readonly IConfiguration config;
+        public static int testing = 0;
+
+        public OthelloController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [HttpGet]
+        [Route("GetAllUsers")]
+        public string GetUsers()
+        {
+            SqlConnection con = new SqlConnection(config.GetConnectionString("OthelloConnection").ToString());
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Users", con);
+
+            testing++;
+            return $"test {testing}";
+        }
+    }
+}
---
+++ OthelloAPI/OthelloAPI/User.cs
@@ -0,0 +1,12 @@
+﻿namespace OthelloAPI
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string? UserName { get; set; }
+        public int GamesPlayed { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Draws { get; set; }
+    }
+}
