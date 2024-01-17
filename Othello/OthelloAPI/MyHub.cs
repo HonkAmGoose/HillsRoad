e72030cdbde5c4cb15efd82f7366d05862e34930
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2024-01-17 10:40:45 +0000

    add OthelloAPI project to Othello solution

---
+++ Othello/OthelloAPI/MyHub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.Server
+{
+    public class MyHub : Hub
+    {
+        public static List<string> messages = new List<string>();
+
+        public string addMessage(string name, string message)
+        {
+            Console.WriteLine($"Server receive: {name} - {message}");
+            
+            Clients.All.addMessage(name, message);
+
+            messages.Add(message);
+
+            return message;
+        }
+
+        public override Task OnConnected()
+        {
+            Console.WriteLine($"Client connected: {Context.ConnectionId}");
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Console.WriteLine($"Client disconnected: {Context.ConnectionId}");
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ Othello/OthelloAPI/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace SignalR.Server //THIS IS ACTUALLY THE SERVER
+{
+    public class Program
+    {
+        private const string Url = "http://localhost:9080";
+
+        public List<string> messages = new List<string>();
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Server started");
+
+            using (WebApp.Start(Url, Configuration))
+            {
+                string message;
+                bool dc = false;
+                var context = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+
+                Console.WriteLine($"Server running on {Url}");
+                Console.WriteLine("Type a message and press enter to send, \"q\" to disconnect");
+
+                do
+                {
+                    message = Console.ReadLine();
+                    if (message.ToLower() == "q")
+                    {
+                        dc = true;
+                        message = "--- SERVER SHUTTING DOWN ---";
+                    }
+
+                    // Send message to all connected clients
+                    context.Clients.All.addMessage("Server", message);
+                } while (dc == false);
+
+                foreach(string _message in MyHub.messages)
+                {
+                    Console.WriteLine(_message);
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        public static void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
