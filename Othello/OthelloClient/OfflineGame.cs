8ca6ccc54c12ce83e4214e528e794226170dea39
HonkAmGoose <73439059+HonkAmGoose@users.noreply.github.com>
2024-01-25 12:20:25 +0000

    add offlineGame BROKEN

---
+++ Othello/OthelloClient/OfflineGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Othello
+{
+    public partial class OfflineGame : Othello.Game
+    {
+        public OfflineGame(Menu parent) : base(parent)
+        {
+            InitializeComponent();
+        }
+    }
+}
